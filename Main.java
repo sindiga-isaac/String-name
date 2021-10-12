0cdeed844a030a7f8e4b4e926a27429b8518fcbe
isaac <59864350+sindiga1@users.noreply.github.com>
2021-10-12 09:41:51 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[]args){
+        String alphabet;
+        alphabet="ABCDEFHSJKHRKJKAJFLIFHJWJASFNEMMALCKSNEIOFN";
+        System.out.println("Length of alphabet is:"+alphabet.length());
+    }
+}
